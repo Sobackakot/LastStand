11f5ab416f345b081315ce1e9cfe28df53dc8dc1
Aibulat <119883177+Sobackakot@users.noreply.github.com>
2024-05-16 15:44:20 +0300

    save

--- Assets/ScriptsC#/WindowUI/GridLayoutGroupPerson.cs
+++ Assets/ScriptsC#/WindowUI/GridLayoutGroupPerson.cs
@@ -1,4 +1,4 @@
- 
+
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,7 +11,7 @@ public class GridLayoutGroupPerson : MonoBehaviour
     private readonly float minCellSize = 100f;
     private readonly float maxCellSize = 170f;
 
-    [Range(1, 18)] private int currentSlotsCount = 1;
+    [Range(1, 18)] private int currentSlotsCount = 1; 
     [Range(100,170)] private float currentCellSize = 100;
 
     private void Start()
@@ -24,16 +24,18 @@ public class GridLayoutGroupPerson : MonoBehaviour
         CharacterSwitchSystem.Instance.onUpdateCellSizeGrid -= Instance_onUpdateCellSizeGrid;
     }
 
-    private void Instance_onUpdateCellSizeGrid()
+    private void Instance_onUpdateCellSizeGrid() // call from CharacterSwitchSystem - ActivePersonUI
     {
         currentSlotsCount = GetCurrentActiveSlot();
         if (currentSlotsCount <6)
         {
+            //if the number of cells is less than 6, then the size will be from 170 to 130
             float cellSize = Mathf.Lerp(minCellSize, maxCellSize, (maxSlots - minSlots) / (currentSlotsCount + minSlots));
             grid.cellSize = new Vector2(cellSize, cellSize);
         }
         else  
         {
+            //if the number of cells is more than six, then the size will be from 130 to 100
             currentCellSize = currentSlotsCount <= 7 ? 130 : 100;
             grid.cellSize = new Vector2(currentCellSize, currentCellSize);
         }
@@ -43,6 +45,7 @@ public class GridLayoutGroupPerson : MonoBehaviour
         currentSlotsCount = 0;
         foreach (Transform child in grid.transform)
         {
+            //counts the current number of active cells in the UI grid
             if (child.gameObject.activeSelf)
             { 
                 currentSlotsCount++;
